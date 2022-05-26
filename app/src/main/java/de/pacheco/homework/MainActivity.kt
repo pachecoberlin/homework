32a90f8738c272a070b8a0678f006157d6f00472
Pacheco <pacheco@gmx.de>
2022-05-26 23:02:05 +0200

    omg

--- app/src/main/java/de/pacheco/homework/MainActivity.kt
+++ app/src/main/java/de/pacheco/homework/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.pacheco.homework
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
@@ -8,11 +9,15 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import de.pacheco.homework.ui.main.SectionsPagerAdapter
 import de.pacheco.homework.databinding.ActivityMainBinding
+import de.pacheco.homework.model.Task
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG: String = "MainActivity"
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +36,31 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            val db =Firebase.firestore
+
+
+//            val task =
+////                Task("test", false, listOf("Monday"),"Pache","do something")
+//            hashMapOf(
+//                "name" to "test", "periodic" to false, "owner" to "Pacheco", "description" to "do something"
+//            )
+//            db.collection("Tasks")
+//                .document()
+//                .set(task)
+//                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+//                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+            val city = hashMapOf(
+                "name" to "Los Angeles",
+                "state" to "CA",
+                "country" to "USA"
+            )
+
+            db.collection("cities").document("LA")
+                .set(city)
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
         }
     }
 }
